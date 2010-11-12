14c39c495f46f6102cd16dd90448ee571c7d4cde
Davy De Waele <ddewaele@gmail.com>
2010-11-12 21:49:33 +0100

    latitude sample

--- src/com/ecs/android/oauth/Constants.java
+++ src/com/ecs/android/oauth/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 	public static final String SCOPE 			= "https://www.googleapis.com/auth/latitude";
 	public static final String REQUEST_URL = "https://www.google.com/accounts/OAuthGetRequestToken";
 	public static final String ACCESS_URL = "https://www.google.com/accounts/OAuthGetAccessToken";  
-	public static final String AUTHORIZE_URL = "https://www.google.com/latitude/apps/OAuthAuthorizeToken?domain=ecommitconsulting.be&location=all&granularity=best&hd=default";
+	public static final String AUTHORIZE_URL = "https://www.google.com/latitude/apps/OAuthAuthorizeToken?domain=anonymous&location=all&granularity=best&hd=default";
 	
 	public static final String API_REQUEST 		= "https://www.googleapis.com/latitude/v1/location";
 	
