d25945b76a878f3329fc9b7e2b6b52e1a74dd1f2
Davy De Waele <ddewaele@gmail.com>
2010-12-11 13:43:47 +0100

    formatting and cleanup

--- src/com/ecs/android/oauth/Constants.java
+++ src/com/ecs/android/oauth/Constants.java
@@ -3,19 +3,17 @@ package com.ecs.android.oauth;
 
 public class Constants {
 
-	public static final String CONSUMER_KEY = "ecommitconsulting.be";
+	public static final String CONSUMER_KEY 			= "ecommitconsulting.be";
 	
-	public static final String SCOPE 			= "https://www.googleapis.com/auth/latitude";
-	public static final String REQUEST_URL = "http://ecommitconsulting.be/docs.php";
+	public static final String SCOPE 					= "https://www.googleapis.com/auth/latitude";
+	public static final String REQUEST_URL 				= "http://ecommitconsulting.be/docs.php";
 	
-	public static final String API_REQUEST 		= "https://www.googleapis.com/latitude/v1/location";
-	
-	public static final String ENCODING 		= "UTF-8";
+	public static final String API_REQUEST 				= "https://www.googleapis.com/latitude/v1/location";
 	
 	public static final String	OAUTH_CALLBACK_SCHEME	= "x-oauthflow-remote";
 	public static final String	OAUTH_CALLBACK_HOST		= "callback";
 	public static final String	OAUTH_CALLBACK_URL		= OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
 
-	public static final String CONSUMER_SECRET_KEY = "consumer_secret";
+	public static final String CONSUMER_SECRET_KEY 		= "consumer_secret";
 
 }
